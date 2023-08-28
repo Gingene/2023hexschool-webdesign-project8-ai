4b36b189debb1e9a74f2edec0c3797ebc9fa49fc
Gingene <ginx2618@gmail.com>
2023-08-28 15:48:27 +0800

    產品頁完成

--- main.js
+++ main.js
@@ -3,66 +3,110 @@ import "bootstrap/dist/js/bootstrap.min.js";
 // import Swiper bundle with all modules installed
 import Swiper from "swiper/bundle";
 import "swiper/css/bundle";
+import productRender from "./assets/js/product";
 
-// init Swiper:
-// const swiper = new Swiper(...);
-const swiper1 = new Swiper(".sw1", {
-  // 分頁、左右箭頭、滾動條若有使用則必需設定
-  // 分頁
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  //
-  slidesPerView: 1,
-  spaceBetween: 24,
+function getCurrentFileName() {
+  let path = window.location.pathname;
+  let fileName = path.split("/").pop();
+  return fileName;
+}
 
-  // 左右箭頭
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+if (getCurrentFileName() === "index.html") {
+  indexHandler();
+}
 
-const swiper2 = new Swiper(".sw2", {
-  // 分頁、左右箭頭、滾動條若有使用則必需設定
-  // 分頁
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  //
-  // slidesPerView: 3,
-  // spaceBetween: 30,
-  breakpoints: {
-    // when window width is >= moblie
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 24,
-    },
-    // when window width is >= pad
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 24,
-    },
-    // when window width is >= pc
-    1200: {
-      slidesPerView: 3,
-      spaceBetween: 24,
-    },
-  },
-  // spaceBetween: 24,
-
-  // 滾動條
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-});
+if (getCurrentFileName() === "product.html") {
+  productHandler();
+  productRender();
+}
 
 console.log("Hello world!");
 
-const text2 = document.querySelector(".text2 p");
-text2.innerHTML = text2.innerText
-  .split("")
-  .map(
-    (char, i) => `<span style="transform:rotate(${i * 20}deg)">${char}</span>`
-  )
-  .join("");
+function indexHandler() {
+  const swiper1 = new Swiper(".sw1", {
+    // 分頁、左右箭頭、滾動條若有使用則必需設定
+    // 分頁
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    //
+    slidesPerView: 1,
+    spaceBetween: 24,
+
+    // 左右箭頭
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  const swiper2 = new Swiper(".sw2", {
+    // 分頁、左右箭頭、滾動條若有使用則必需設定
+    // 分頁
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    //
+    // slidesPerView: 3,
+    // spaceBetween: 30,
+    breakpoints: {
+      // when window width is >= moblie
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 24,
+      },
+      // when window width is >= pad
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 24,
+      },
+      // when window width is >= pc
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+    },
+    // spaceBetween: 24,
+
+    // 滾動條
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
+
+  const text2 = document.querySelector(".text2 p");
+  text2.innerHTML = text2.innerText
+    .split("")
+    .map(
+      (char, i) => `<span style="transform:rotate(${i * 20}deg)">${char}</span>`
+    )
+    .join("");
+  console.log(swiper2, text2);
+}
+
+function productHandler() {
+  const swiper3 = new Swiper(".sw3", {
+    // 分頁、左右箭頭、滾動條若有使用則必需設定
+    // 分頁
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    //
+    slidesPerView: 1,
+    spaceBetween: 0,
+
+    // 左右箭頭
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  const text1 = document.querySelector(".text1 p");
+  text1.innerHTML = text1.innerText
+    .split("")
+    .map(
+      (char, i) => `<span style="transform:rotate(${i * 20}deg)">${char}</span>`
+    )
+    .join("");
+}
